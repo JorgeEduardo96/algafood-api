db6ca493669c84155136517fbc50a76f7dc69c51
Jorge Eduardo Primo <jorge.primo@liferay.com>
2021-09-03 17:58:37 -0300

    Primeiro commit.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into cozinha (id, nome) values (1, 'Tailandesa');
+insert into cozinha (id, nome) values (2, 'Indiana');
+
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Muay Thai Restaurante', 7.50, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Restaurante Dragão Branco', 12.00, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Arebaba Hamburgueria', 5.75, 2);
